c86a9aac697a85dc28d48e79ef82a6cbe8224994
Dominik Košut <kosut.dom@gmail.com>
2022-06-01 11:39:36 +0200

    Vytvořeni kostry pro ownera

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,28 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { UserComponent } from './user/user.component';
+import { UsersComponent } from './users/users.component';
+import { UserListComponent } from './users/user-list/user-list.component';
+import { UserItemComponent } from './users/user-list/user-item/user-item.component';
+import { UserDetailComponent } from './users/user-detail/user-detail.component';
+import { UserEditComponent } from './users/user-edit/user-edit.component';
+import { CarsComponent } from './cars/cars.component';
+import { DetailsComponent } from './details/details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    UserComponent,
+    UsersComponent,
+    UserListComponent,
+    UserItemComponent,
+    UserDetailComponent,
+    UserEditComponent,
+    CarsComponent,
+    DetailsComponent
   ],
   imports: [
     BrowserModule
