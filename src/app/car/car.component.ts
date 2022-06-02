558af98b5b4ccd0dac72304c7471832a3132f986
Dominik Košut <kosut.dom@gmail.com>
2022-06-02 15:29:36 +0200

    cars hotovo

--- src/app/car/car.component.ts
+++ src/app/car/car.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+import { from } from 'rxjs';
+import { Car } from '../cars/car.module';
+import { CarsService } from '../cars/cars.service';
 
 @Component({
   selector: 'app-car',
@@ -6,10 +11,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./car.component.css']
 })
 export class CarComponent implements OnInit {
+  constructor(private router: Router,
+              private carsServ: CarsService) { }
 
-  constructor() { }
+  @ViewChild('f') form: NgForm;
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  onSubmit(){
+  console.log(this.form);
+
+  this.carsServ.addNewCar(
+    new Car(0, this.form.value.type, this.form.value.color, this.form.value.fuel) 
+    ).subscribe({
+      next: () => {
+      this.form.reset();
+      this.router.navigate(['/cars']);
+      this.carsServ.carChange.next(true);
+      }
+    });
   }
 
 }
